33e48faceab71c04971b7ca053793b6a53aa8570
Almu Iturgoyen <46594025+almu-itur@users.noreply.github.com>
2022-07-12 16:45:42 +0200

    add env var to webpack serve command

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV,
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
